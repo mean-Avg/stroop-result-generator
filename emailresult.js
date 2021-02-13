faddc4f602b71e5acac1e17b595ebf6d74dcae31
Aditya Vardhan Gupta <1994.avg@gmail.com>
2021-02-14 00:16:54 +0530

    nodemailer working

---
+++ emailresult.js
@@ -0,0 +1,23 @@
+var nodemailer = require("nodemailer");
+var credentials = require("./credentials.js");
+
+module.exports = function (req) {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: credentials.username,
+      pass: credentials.pass,
+    },
+  });
+  var mailOptions = {
+    from: credentials.username,
+    to: req.body.email,
+    subject: "Test email",
+    text: req.body.uniquecode,
+  };
+  console.log("Email servive commented right now");
+  //   transporter.sendMail(mailOptions, function (error, info) {
+  //     if (error) console.log(error);
+  //     else console.log("Email sent:" + info.response);
+  //   });
+};
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+const emailresult = require("./emailresult.js");
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/requestResults", (req, res) => {
+  emailresult(req);
+  res.send("ok");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at port ${port}`)
-})
+  console.log(`Example app listening at port ${port}`);
+});
